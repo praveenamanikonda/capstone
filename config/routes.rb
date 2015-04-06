aa9fb29c7748ff4bcb30d2d14a4f6991d55eb555
praveenamanikonda <praveena.manikonda@gmail.com>
2015-04-06 19:19:38 +0000

    Finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
   
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
 
   resources :microposts
   resources :users
